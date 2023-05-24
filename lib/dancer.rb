10ba888023aed4cf1a7d38f3099e3bbb6a0e17dc
Eric Mugo <mugoericpg20@gmail.com>
2023-05-24 15:20:09 +0300

    Include

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,7 +1,14 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance.rb'
 
 class Dancer
-    extend MetaDancing
     include Dance
-end
+    
+  
+    attr_accessor :name
+  
+    def initialize(name)
+      @name = name
+    end
+  end
--- lib/kid.rb
+++ lib/kid.rb
@@ -2,6 +2,11 @@ require_relative './dance_module'
 require_relative './meta_dancing_module'
 
 class Kid
-    extend MetaDancing
     include Dance
-end
+  
+    attr_accessor :name
+  
+    def initialize(name)
+      @name = name
+    end
+  end
